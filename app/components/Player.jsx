1254d0d646f5d1c70f1d5321c6bd6b9c5e959945
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-09-22 11:32:50 -0700

    volume functionality added

--- app/components/Player.jsx
+++ app/components/Player.jsx
@@ -6,15 +6,20 @@ import { useDispatch } from 'react-redux';
 import { useSound } from 'use-sound';
 import { BsPlayCircleFill, BsPauseCircleFill } from "react-icons/bs";
 import { BiSolidLike, BiLike } from "react-icons/bi";
-
+import { FaVolumeMute } from "react-icons/fa"
+import { VscUnmute } from "react-icons/vsc";
 const Player = () => {
   const dispatch = useDispatch();
   const [playing, setPlaying] = useState(true);
   const [position, setPosition] = useState(0); // Track current playback position
 
+  const [volume, setVolume] = useState(0.6); // 1 is full volume, 0 is muted
+  const [muted, setMuted] = useState(false);
+
   const currentSong = useAppSelector((state) => state?.user?.currentSong);
   const [play, { pause, stop }] = useSound(currentSong?.song, {
     position: position, // Set the initial playback position
+    volume: volume
   });
 
 
@@ -51,20 +56,63 @@ const Player = () => {
   const handleStart = async () => {
     if (playing) {
       stop();
+      setPosition(0); // Reset the position when stopped
+      setPlaying(false);
+    }
+    else {
+      stop();
+      setPosition(0); // Reset the position when stopped
+      setPlaying(false);
     }
     play();
+    setPlaying(true);
   }
 
-  const handleClose = async() => {
-   
-    await dispatch(removeCurrentSong());
-    
-  }
+
+
+
+
 
   useEffect(() => {
     handleStop();
-    
+
   }, [currentSong])
+
+  const handleVolumeChange = (event) => {
+    // if (muted === false) {
+      const newVolume = parseFloat(event.target.value);
+      setVolume(newVolume);
+      console.log("volume updated to ", newVolume);
+      setMuted(false);
+    // }
+    // else {
+    //   return;
+    // }
+
+  };
+
+  const toggleMuted = () => {
+    if(muted===true){
+      setMuted(false);
+      setVolume(0.6)
+
+    }
+    else {
+      setMuted(true);
+      setVolume(0)
+    }
+  }
+
+  useEffect(() => {
+   if(volume ==0 ){
+    setMuted(true);
+   }
+   else {
+    setMuted(false);
+   }
+  }, [volume])
+  
+
 
 
   return (
@@ -79,20 +127,46 @@ const Player = () => {
               <BiLike className="cursor-pointer mt-2 text-4xl rounded-full  hover:text-green-300 ml-2 text-green-500" />
             </div>
           </div>
-          <div className='flex h-full w-1/3 justify-center items-center'>
-            <AiFillStepBackward className="cursor-pointer mt-2 text-4xl rounded-full  hover:text-green-300 ml-2 text-green-500" onClick={handleStart} />
-            {playing ? (
-              <BsPauseCircleFill className="cursor-pointer mt-2 text-4xl rounded-full  hover:text-green-300 ml-2 text-green-500" onClick={handlePause} />
-            ) : (
-              <BsPlayCircleFill className="cursor-pointer mt-2 text-4xl rounded-full  hover:text-green-300 ml-2 text-green-500" onClick={handlePlay} />
-            )}
-            <AiFillStepForward className="cursor-pointer mt-2 text-4xl rounded-full  hover:text-green-300 ml-2 text-green-500" />
-          </div>
-          <div className="flex flex-row-reverse justify-end h-full w-1/3 ">
-            <div className="ml-auto">
-            <AiOutlineClose className="cursor-pointer mt-2 text-4xl rounded-full  hover:text-green-300 ml-2 text-green-500" onClick={handleClose} />
+          <div className='flex flex-col h-full w-1/3 justify-center items-center'>
 
+            <div className=' flex w-full h-3/4 justify-center items-center'>
+              <AiFillStepBackward className="cursor-pointer mt-2 text-4xl rounded-full  hover:text-green-300 ml-2 text-green-500" onClick={handleStart} />
+              {playing ? (
+                <BsPauseCircleFill className="cursor-pointer mt-2 text-4xl rounded-full  hover:text-green-300 ml-2 text-green-500" onClick={handlePause} />
+              ) : (
+                <BsPlayCircleFill className="cursor-pointer mt-2 text-4xl rounded-full  hover:text-green-300 ml-2 text-green-500" onClick={handlePlay} />
+              )}
+              <AiFillStepForward className="cursor-pointer mt-2 text-4xl rounded-full  hover:text-green-300 ml-2 text-green-500" />
             </div>
+
+            <div className='w-full h-1/4 mt-2 '>
+              {/* the slider functionality */}
+            </div>
+
+
+
+          </div>
+          <div className="flex flex-row justify-start items-center h-full w-1/3 ">
+            {muted === true &&
+              <FaVolumeMute onClick={toggleMuted} className="cursor-pointer mr-2 text-2xl rounded-full  hover:text-green-300 ml-2 text-green-500" />
+
+            }
+            {
+              muted === false &&
+              <VscUnmute onClick={toggleMuted} className="cursor-pointer mr-2 text-2xl rounded-full  hover:text-green-300 ml-2 text-green-500" />
+
+            }
+
+            <input
+              className='bg-green-500 appearance-none w-1/3 h-1 rounded-full outline-none focus:ring focus:ring-green-500'
+              type="range"
+              min={0}
+              max={1}
+              step={0.01}
+              value={volume}
+              onChange={handleVolumeChange}
+            />
+
           </div>
 
         </div>
