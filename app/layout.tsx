cd66863a2028c27c5cff111a40bace8a1c9bdab0
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-09-27 12:01:00 -0700

    d

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { store } from './redux/store';
 //   title: 'Spotify',
 //   description: 'Enjoy your favourite songs on My spotify ',
 // }
-
+//fine
 export default function RootLayout({
   children,
 }: {
@@ -25,8 +25,6 @@ export default function RootLayout({
         <div className='h-screen w-full bg-black flex'>
         <div className='h-full w-1/5  md:flex-col lg:flex-col'> <Sidebar /> </div>
           <div className='h-full w-4/5 ml-4 m-2'>{children}</div>
-         
-
         </div>
        
         </body>
